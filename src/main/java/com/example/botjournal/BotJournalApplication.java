13fd7809d80f449edc410fd84fb025bb6352f390
Latifus <saitbaevlatif@gmail.com>
2023-10-05 10:45:37 +0200

    init commit

---
+++ src/main/java/com/example/botjournal/BotJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.botjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotJournalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotJournalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/botjournal/BotJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.botjournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotJournalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
